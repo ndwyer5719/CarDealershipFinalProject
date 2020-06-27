67171a04b9574ae06c89e738787be69be14fb012
ndwyer5719 <67170639+ndwyer5719@users.noreply.github.com>
2020-06-26 22:59:50 -0400

    Create Users.java

---
+++ Users.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class User {
+    public int idNumber;
+    public String FName;
+    public String LName;
+    public String Birthday;
+    public String Address;
+    public Integer PhoneNumber;
+    public Integer SSN;
+    public Boolean Active;
+}
